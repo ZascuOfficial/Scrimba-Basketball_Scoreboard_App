11f09a3df8e9e2021ddaca80efc4bdf6d45ee1be
Zascu <zascu.github@gmail.com>
2022-10-29 16:11:29 +0300

    Refactor the 6 functions into 2, with arguments

--- js/main.js
+++ js/main.js
@@ -4,42 +4,14 @@ const secondScoreEl = document.getElementById('second-score');
 let firstScoreNum = 0;
 let secondScoreNum = 0;
 
-// Functions for the first set of buttons
-
-function firstAdd1() {
-  firstScoreNum++;
-  
-  firstScoreEl.textContent = firstScoreNum;
-}
-
-function firstAdd2() {
-  firstScoreNum += 2;
+function firstAdd(value) {
+  firstScoreNum += value;
 
   firstScoreEl.textContent = firstScoreNum;
 }
 
-function firstAdd3() {
-  firstScoreNum += 3;
-
-  firstScoreEl.textContent = firstScoreNum;
-}
-
-// Functions for the second set of buttons
-
-function secondAdd1() {
-  secondScoreNum++;
-
-  secondScoreEl.textContent = secondScoreNum;
-}
-
-function secondAdd2() {
-  secondScoreNum += 2;
-
-  secondScoreEl.textContent = secondScoreNum;
-}
-
-function secondAdd3() {
-  secondScoreNum += 3;
+function secondAdd(value) {
+  secondScoreNum += value;
 
   secondScoreEl.textContent = secondScoreNum;
 }
